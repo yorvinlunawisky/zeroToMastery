e67de51d7cb4397d1b0ed6fddff21176b6e5cb85
Yorvin Luna Wisky <yorvin.luna@micm.gob.do>
2022-05-13 15:17:56 -0400

    button added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular basics';
- }
+
+  imgLink: string = "https://images.unsplash.com/photo-1648737119359-510d4f551382?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80";
+
+  imgChanged(event: any) {
+    this.imgLink = event;
+    console.log(event)
+  }
+}
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -6,6 +6,12 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent {
+  newImageUrl: string = "https://images.unsplash.com/photo-1638913658642-8f22bae3335b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80";
 
+  @Output() newImage =  new EventEmitter<any>();
+
+  buttonClicked() {
+    this.newImage.emit(this.newImageUrl);
+  }
 }
  
