8e5fee43abc76caa8b8a04ad5e3cbabdf2212d4a
Yorvin Luna Wisky <yorvin.luna@micm.gob.do>
2022-05-13 14:35:31 -0400

    Adding pipes

---
+++ src/app/products/products.component.ts
@@ -0,0 +1,11 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent {
+
+}
+ 
