90ef16ba34379fd87f87e7f9d6f8141e06f26b9a
Yorvin Luna Wisky <yorvin.luna@micm.gob.do>
2022-05-13 15:38:23 -0400

    Having problem with the EventEmitter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HtmlParser } from '@angular/compiler';
 import { Component } from '@angular/core';
 
 @Component({
@@ -11,7 +12,6 @@ export class AppComponent {
   imgLink: string = "https://images.unsplash.com/photo-1648737119359-510d4f551382?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80";
 
   imgChanged(event: any) {
-    this.imgLink = event;
-    console.log(event)
+    console.log();
   }
 }
